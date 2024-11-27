4bbaad5ece28be6fcb510ac5ba638616097ca3c3
Pongsiri chatkaew <pongsiri.cha@hotmail.com>
2024-11-27 20:21:06 +0700

    feat: add my blog page

---
+++ app/posts/me/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Post, { PostItem } from '@/app/posts/components/PostCard';
+import { usePosts } from '@/contexts/PostContext';
+import { useEffect } from 'react';
+import AddPostModal from '../components/AddPostModal';
+
+export default function PostsMePage() {
+  const { posts, fetchPosts } = usePosts();
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className='flex flex-col space-y-1 p-8'>
+      <div className='flex flex-row-reverse py-4 items-end gap-4'>
+        <AddPostModal />
+      </div>
+
+      {posts.map((post: PostItem) => (
+        <Post
+          key={post.id}
+          postId={post.id}
+          username={post.user.username}
+          community={post.community}
+          title={post.title}
+          content={post.content}
+          commentCount={post.comments.length}
+          editable
+        />
+      ))}
+    </div>
+  );
+}
